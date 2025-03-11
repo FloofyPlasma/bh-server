0a17271328850f1b374d1e18e6d2409d70f28c49
FloofyPlasma <github@floofyplasma.com>
2025-03-11 02:04:22 -0400

    feat: partial action initWithSaveDict...

--- src/SharedSource/Action.mm
+++ src/SharedSource/Action.mm
@@ -60,6 +60,30 @@
 
 - (Action*)initWithSaveDict:(NSDictionary*)saveDict inventoryItems:(NSArray*)inventoryItems
 {
+  self = [super init];
+
+  if (self != NULL) {
+    self->inProgress = [[saveDict objectForKey:@"inProgress"] boolValue];
+    self->isAI = [[saveDict objectForKey:@"isAI"] boolValue];
+    self->goalTilePos = makeIntpair([[saveDict objectForKey:@"goalTilePos.x"] intValue], [[saveDict objectForKey:@"goalTilePos.y"] intValue]);
+    self->interactionItemIndex = [[saveDict objectForKey:@"interactionItemIndex"] intValue];
+    self->interactionItemSubIndex = [[saveDict objectForKey:@"interactionItemSubIndex"] intValue];
+
+    //! TODO: Finish this section, it does something with self->animationTimer.
+
+    [self->interactionItem retain];
+    self->goalInteraction = [[saveDict objectForKey:@"goalInteraction"] intValue];
+    self->pathType = [[saveDict objectForKey:@"pathType"] intValue];
+    self->interactionObjectID = [[saveDict objectForKey:@"interactionObjectID"] unsignedLongLongValue];
+
+    //! TODO: Finish this section, it does something ith self->craftableItemObject.
+
+    self->craftCountOrExtraData = [[saveDict objectForKey:@"craftCountOrExtraData"] unsignedLongValue];
+    self->inventoryChange = [[saveDict objectForKey:@"inventoryChange"] retain];
+    [[saveDict objectForKey:@"interactionTestResult"] getBytes:&self->interactionTestResult length:12];
+  }
+
+  return self;
 }
 
 - (Action*)initWithGoalPos:(intpair)goalTilePos_ goalInteraction:(int)goalInteraction_ pathType:(int)pathType_ interactionItem:(InventoryItem*)interactionItem_ itemIndex:(uint16_t)interactionItemIndex_ itemSubIndex:(uint16_t)interactionSubItemIndex_ interactionObjectID:(uint64_t)interactionObjectID_ craftableItemObject:(CraftableItemObject*)craftableItemObject_ craftCountOrExtraData:(uint16_t)craftCountOrExtraData_ isAI:(bool)isAI_ inventoryChange:(NSDictionary*)inventoryChange_
