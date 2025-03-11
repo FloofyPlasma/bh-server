2c61a6ee8344ebd86e5b7a3216196187b23732dd
FloofyPlasma <github@floofyplasma.com>
2025-03-11 02:13:09 -0400

    fix: clangd bitching >:(

--- src/SharedSource/Vector2.h
+++ src/SharedSource/Vector2.h
@@ -5,13 +5,15 @@ class Vector2 {
   float _vector[2];
 
   Vector2(float x, float y)
-      : _vector { x, y }
   {
+    _vector[0] = x;
+    _vector[1] = y;
   }
 
   Vector2()
-      : _vector { 1.0f, 1.0f }
   {
+    _vector[0] = 1.0f;
+    _vector[1] = 1.0f;
   }
 
   float lengthSquared()
@@ -30,20 +32,20 @@ class Vector2 {
 
   Vector2 operator*(Vector2 vec)
   {
-    return { _vector[0] * vec._vector[0], _vector[1] * vec._vector[1] };
+    return Vector2(_vector[0] * vec._vector[0], _vector[1] * vec._vector[1]);
   }
 
-  Vector2 operator*(float val) { return { _vector[0] * val, _vector[1] * val }; }
+  Vector2 operator*(float val) { return Vector2(_vector[0] * val, _vector[1] * val); }
 
   Vector2 operator+(Vector2 vec)
   {
-    return { _vector[0] + vec._vector[0], _vector[1] + vec._vector[1] };
+    return Vector2(_vector[0] + vec._vector[0], _vector[1] + vec._vector[1]);
   }
 
   Vector2 operator-(Vector2 vec)
   {
-    return { _vector[0] - vec._vector[0], _vector[1] - vec._vector[1] };
+    return Vector2(_vector[0] - vec._vector[0], _vector[1] - vec._vector[1]);
   }
 
-  Vector2 operator/(float val) { return { _vector[0] / val, _vector[1] / val }; }
+  Vector2 operator/(float val) { return Vector2(_vector[0] / val, _vector[1] / val); }
 };
