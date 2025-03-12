7043be872e377cb46f3a99b5da4dad059ffcf50e
FloofyPlasma <github@floofyplasma.com>
2025-03-11 23:12:20 -0400

    feat: implement some gl logic

---
+++ src/Linux/GLKit/GLKit/GLKMathUtils.mm
@@ -0,0 +1,14 @@
+#import "GLKit/GLKMathTypes.h"
+#import <Foundation/Foundation.h>
+
+GLKVector3 GLKMathProject(GLKVector3 object, GLKMatrix4 model, GLKMatrix4 projection, int* viewport)
+{
+  [NSException raise:NSRangeException
+              format:@"Not implemented."];
+}
+
+GLKVector3 GLKMathUnproject(GLKVector3 window, GLKMatrix4 model, GLKMatrix4 projection, int* viewport, BOOL* success)
+{
+      [NSException raise:NSRangeException
+              format:@"Not implemented."];
+}
