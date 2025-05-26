9cb00c061d02a2457921b1028037481f3f400833
FloofyPlasma <github@floofyplasma.com>
2025-05-26 17:33:45 -0400

    fix: mj sound object types
    #5

--- src/SharedSource/MJSound.h
+++ src/SharedSource/MJSound.h
@@ -36,32 +36,32 @@
     hasMultiSoundPlayBackFrequency; // @synthesize
 
 - (BOOL)randomNumberWithinMultiPlaybackFrequency;
-- (void)playAtPosition:(Vector2)arg1;
-- (void)setPosition:(Vector2)arg1;
-- (void)setPlaybackFrequencyInMultiSound:(float)arg1;
+- (void)playAtPosition:(Vector2)pos;
+- (void)setPosition:(Vector2)pos;
+- (void)setPlaybackFrequencyInMultiSound:(float)multiSoundPlayBackFrequency_;
 - (BOOL)paused;
-- (id)sound;
-- (void)setMasterVolumeMultiplier:(float)arg1;
+- (MJSound*)sound;
+- (void)setMasterVolumeMultiplier:(float)multiplier;
 - (BOOL)isInUse;
-- (void)setInUse:(BOOL)arg1;
+- (void)setInUse:(BOOL)inUse;
 - (BOOL)isPlaying;
 - (BOOL)hasFinishedPlaying;
-- (void)setPitch:(float)arg1;
+- (void)setPitch:(float)pitch;
 - (float)getLegacyVolume;
-- (void)setVolume:(float)arg1;
-- (void)setLegacyVolume:(float)arg1;
-- (void)setLooping:(BOOL)arg1;
-- (void)update:(float)arg1;
+- (void)setVolume:(float)volume;
+- (void)setLegacyVolume:(float)volume;
+- (void)setLooping:(BOOL)looping;
+- (void)update:(float)dt;
 - (void)pause;
-- (void)setPaused:(BOOL)arg1;
-- (void)fadeOut:(float)arg1;
+- (void)setPaused:(BOOL)paused;
+- (void)fadeOut:(float)duration;
 - (void)stop;
-- (void)playAfterDelay:(float)arg1;
+- (void)playAfterDelay:(float)delay;
 - (void)play;
-- (void)setXOffset:(float)arg1;
+- (void)setXOffset:(float)xOffset;
 - (void)dealloc;
-- (id)initWithSound:(id)arg1;
-- (id)fileName;
-- (id)initWithFile:(id)arg1;
+- (MJSound*)initWithSound:(MJSound*)sound;
+- (NSString*)fileName;
+- (MJSound*)initWithFile:(NSString*)file;
 
 @end
