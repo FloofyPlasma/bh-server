c7c6eeb665b3b8bbd8bd1b53edc6667ed6a7f4b1
FloofyPlasma <github@floofyplasma.com>
2025-05-26 18:17:21 -0400

    fix: passenger car object type
    #5

--- src/SharedSource/PassengerCar.h
+++ src/SharedSource/PassengerCar.h
@@ -16,44 +16,45 @@ struct PassengerCarCreationNetData {
   DrawCube* roofCube;
 }
 
-- (void)addRider:(id)arg1;
+- (void)addRider:(Blockhead*)blockhead;
 - (int)maxNumberOfRiders;
-- (BOOL)tapIsWithinBodyRadius:(Vector2)arg1;
-- (id)actionTitle;
-- (int)itemType;
-- (void)setTargetVelocity:(Vector2)arg1;
-- (GLKMatrix4)riderBodyMatrixForBlockhead:(id)arg1 cameraX:(float)arg2;
-- (Vector)riderPosForBlockhead:(id)arg1;
-- (Vector2)cameraPosForBlockhead:(id)arg1;
+- (BOOL)tapIsWithinBodyRadius:(Vector2)tapLocation;
+- (NSString*)actionTitle;
+- (ItemType)itemType;
+- (void)setTargetVelocity:(Vector2)targetVelocity;
+- (GLKMatrix4)riderBodyMatrixForBlockhead:(Blockhead*)blockhead
+                                  cameraX:(float)cameraX;
+- (Vector)riderPosForBlockhead:(Blockhead*)blockhead;
+- (Vector2)cameraPosForBlockhead:(Blockhead*)blockhead;
 - (Vector2)renderPos;
-- (void)draw:(float)arg1
-    projectionMatrix:(GLKMatrix4)arg2
-     modelViewMatrix:(GLKMatrix4)arg3
-     cameraMinXWorld:(int)arg4
-     cameraMaxXWorld:(int)arg5
-     cameraMinYWorld:(int)arg6
-     cameraMaxYWorld:(int)arg7;
-- (void)update:(float)arg1 accurateDT:(float)arg2 isSimulation:(BOOL)arg3;
-- (void)remoteUpdate:(id)arg1;
+- (void)draw:(float)dt
+    projectionMatrix:(GLKMatrix4)projectionMatrix
+     modelViewMatrix:(GLKMatrix4)modelViewMatrix
+     cameraMinXWorld:(int)cameraMinXWorld
+     cameraMaxXWorld:(int)cameraMaxXWorld
+     cameraMinYWorld:(int)cameraMinYWorld
+     cameraMaxYWorld:(int)cameraMaxYWorld;
+- (void)update:(float)dt accurateDT:(float)accurateDT isSimulation:(BOOL)isSimulation;
+- (void)remoteUpdate:(NSData*)netData;
 - (void)dealloc;
-- (id)updateNetDataForClient:(id)arg1;
-- (id)creationNetDataForClient:(id)arg1;
-- (id)getSaveDict;
-- (id)initWithWorld:(id)arg1
-       dynamicWorld:(id)arg2
-              cache:(id)arg3
-            netData:(id)arg4;
-- (id)initWithWorld:(id)arg1
-       dynamicWorld:(id)arg2
-           saveDict:(id)arg3
-              cache:(id)arg4;
-- (id)initWithWorld:(id)arg1
-       dynamicWorld:(id)arg2
-         atPosition:(intpair)arg3
-              cache:(id)arg4
-           saveDict:(id)arg5
-     placedByClient:(id)arg6;
-- (int)objectType;
+- (NSData*)updateNetDataForClient:(NSString*)clientID;
+- (NSData*)creationNetDataForClient:(NSString*)clientID;
+- (NSMutableDictionary*)getSaveDict;
+- (PassengerCar*)initWithWorld:(World*)world_
+                  dynamicWorld:(DynamicWorld*)dynamicWorld
+                         cache:(CPCache*)cache_
+                       netData:(NSData*)netData;
+- (PassengerCar*)initWithWorld:(World*)world_
+                  dynamicWorld:(DynamicWorld*)dynamicWorld
+                      saveDict:(NSDictionary*)saveDict
+                         cache:(CPCache*)cache_;
+- (PassengerCar*)initWithWorld:(World*)world_
+                  dynamicWorld:(DynamicWorld*)dynamicWorld
+                    atPosition:(intpair)pos
+                         cache:(CPCache*)cache_
+                      saveDict:(NSDictionary*)saveDict
+                placedByClient:(NSString*)clientId;
+- (DynamicObjectType)objectType;
 - (void)loadDerivedStuff;
 
 @end
