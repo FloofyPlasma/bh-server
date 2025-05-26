0542369383f74c9ba89e25eef8dd0d8c221afc49
FloofyPlasma <github@floofyplasma.com>
2025-05-26 13:50:33 -0400

    fix: donkey object type

--- src/SharedSource/Donkey.h
+++ src/SharedSource/Donkey.h
@@ -1,24 +1,25 @@
 #import <Foundation/Foundation.h>
 
 #import "DonkeyLike.h"
+#import "Plant.h"
 
 @class CPTexture2D, DrawCube;
 
 @interface Donkey : DonkeyLike {
   CPTexture2D* earTexture;
   DrawCube* earCube;
-  union _GLKMatrix4 leftEarMatrix;
-  union _GLKMatrix4 rightEarMatrix;
+  GLKMatrix4 leftEarMatrix;
+  GLKMatrix4 rightEarMatrix;
   CPTexture2D* hornTexture;
   DrawCube* hornCube;
-  union _GLKMatrix4 hornMatrix;
+  GLKMatrix4 hornMatrix;
   DrawCube* tailCubeA;
   DrawCube* tailCubeB;
   DrawCube* tailCubeC;
-  union _GLKMatrix4 tailMatrixA;
-  union _GLKMatrix4 tailMatrixB;
-  union _GLKMatrix4 tailMatrixC;
-  union _GLKMatrix4 tailMatrixD;
+  GLKMatrix4 tailMatrixA;
+  GLKMatrix4 tailMatrixB;
+  GLKMatrix4 tailMatrixC;
+  GLKMatrix4 tailMatrixD;
   float emitTimer;
   float lastYPosForTailMovement;
   float lastTailExtraRotation;
@@ -26,31 +27,31 @@
   float lastTailExtraRotationC;
 }
 
-- (BOOL)blockheadCanRide:(id)arg1 usingItem:(int)arg2;
-- (id)breedString;
+- (BOOL)blockheadCanRide:(Blockhead*)blockhead usingItem:(ItemType)itemType;
+- (NSString*)breedString;
 - (int)generateBreedForChild;
 - (void)createItemDropsForDeath;
-- (void)drawSubClassStuff:(float)arg1
-         projectionMatrix:(union _GLKMatrix4)arg2
-          modelViewMatrix:(union _GLKMatrix4)arg3;
-- (void)setupMatrices:(Vector2)arg1 dt:(float)arg2;
+- (void)drawSubClassStuff:(float)dt
+         projectionMatrix:(GLKMatrix4)projectionMatrix
+          modelViewMatrix:(GLKMatrix4)modelViewMatrix;
+- (void)setupMatrices:(Vector2)renderPos dt:(float)dt;
 - (float)maxVelocity;
 - (BOOL)galloping;
 - (BOOL)canJumpMultipleTilesWhileFlying;
 - (BOOL)flies;
-- (unsigned short)maxHealth;
+- (uint16_t)maxHealth;
 - (void)dealloc;
 - (void)loadDerivedStuff;
-- (unsigned long long)creationDataStructSize;
+- (uint64_t)creationDataStructSize;
 - (int)getNamesArrayCount;
-- (id*)getNamesArray;
-- (int)captureRequiredItemType;
-- (int)capturedItemType;
-- (int)foodItemType;
-- (id)speciesName;
-- (int)foodPlantType;
+- (NSString**)getNamesArray;
+- (ItemType)captureRequiredItemType;
+- (ItemType)capturedItemType;
+- (ItemType)foodItemType;
+- (NSString*)speciesName;
+- (PlantType)foodPlantType;
 - (float)minFullness;
 - (float)maxAge;
-- (int)npcType;
+- (NPCType)npcType;
 
 @end
