205e47b0c4807bd5a12312ef97cf7a2dbc4703c7
FloofyPlasma <github@floofyplasma.com>
2025-05-26 15:58:25 -0400

    fix: fire object types
    #5

--- src/SharedSource/FireObject.h
+++ src/SharedSource/FireObject.h
@@ -18,38 +18,38 @@ struct FireCreationNetData {
   float animationLoopTimer;
 }
 
-- (void)addArtificialLightContributionForPhysicalBlockLoadedAtXPos:(int)arg1
-                                                              yPos:(int)arg2;
-- (void)worldChanged:(std::vector<intpair>*)arg1;
-- (void)draw:(float)arg1
-    projectionMatrix:(GLKMatrix4)arg2
-     modelViewMatrix:(GLKMatrix4)arg3
-     cameraMinXWorld:(int)arg4
-     cameraMaxXWorld:(int)arg5
-     cameraMinYWorld:(int)arg6
-     cameraMaxYWorld:(int)arg7;
-- (void)update:(float)arg1 accurateDT:(float)arg2 isSimulation:(BOOL)arg3;
+- (void)addArtificialLightContributionForPhysicalBlockLoadedAtXPos:(int)macroX
+                                                              yPos:(int)macroY;
+- (void)worldChanged:(std::vector<intpair>*)worldChangedPositions;
+- (void)draw:(float)dt
+    projectionMatrix:(GLKMatrix4)projectionMatrix
+     modelViewMatrix:(GLKMatrix4)modelViewMatrix
+     cameraMinXWorld:(int)cameraMinXWorld
+     cameraMaxXWorld:(int)cameraMaxXWorld
+     cameraMinYWorld:(int)cameraMinYWorld
+     cameraMaxYWorld:(int)cameraMaxYWorld;
+- (void)update:(float)dt accurateDT:(float)accurateDT isSimulation:(BOOL)isSimulation;
 - (BOOL)requiresPhysicalBlock;
 - (void)removeFromMacroBlock;
-- (void)setNeedsRemoved:(BOOL)arg1;
+- (void)setNeedsRemoved:(BOOL)needsRemoved;
 - (void)dealloc;
-- (id)creationNetDataForClient:(id)arg1;
-- (id)updateNetDataForClient:(id)arg1;
-- (id)getSaveDict;
-- (id)initWithWorld:(id)arg1
-       dynamicWorld:(id)arg2
-              cache:(id)arg3
-            netData:(id)arg4;
-- (id)initWithWorld:(id)arg1
-       dynamicWorld:(id)arg2
-           saveDict:(id)arg3
-              cache:(id)arg4;
-- (id)initWithWorld:(id)arg1
-       dynamicWorld:(id)arg2
-         atPosition:(intpair)arg3
-              cache:(id)arg4;
+- (NSData*)creationNetDataForClient:(NSString*)clientID;
+- (NSData*)updateNetDataForClient:(NSString*)clientID;
+- (NSMutableDictionary*)getSaveDict;
+- (FireObject*)initWithWorld:(World*)world_
+                dynamicWorld:(DynamicWorld*)dynamicWorld
+                       cache:(CPCache*)cache_
+                     netData:(NSData*)netData;
+- (FireObject*)initWithWorld:(World*)world_
+                dynamicWorld:(DynamicWorld*)dynamicWorld
+                    saveDict:(NSDictionary*)saveDict
+                       cache:(CPCache*)cache_;
+- (FireObject*)initWithWorld:(World*)world_
+                dynamicWorld:(DynamicWorld*)dynamicWorld
+                  atPosition:(intpair)pos
+                       cache:(CPCache*)cache_;
 - (Vector)getLightRGB;
-- (int)objectType;
+- (DynamicObjectType)objectType;
 - (void)initSubDerivedItems;
 
 @end
