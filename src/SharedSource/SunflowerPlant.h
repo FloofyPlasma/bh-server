2b48d6ff7fe98b638941387366b3491375f2979f
FloofyPlasma <github@floofyplasma.com>
2025-05-26 19:10:38 -0400

    fix: sunflower object types
    #5

--- src/SharedSource/SunflowerPlant.h
+++ src/SharedSource/SunflowerPlant.h
@@ -5,17 +5,17 @@
 @interface SunflowerPlant : NormalPlant {
 }
 
-- (int)renderImageType;
-- (BOOL)isRequiredSoilType:(int)arg1;
-- (int)plantType;
-- (BOOL)canDieSeason:(int)arg1;
-- (BOOL)floweringSeason:(int)arg1;
+- (ImageType)renderImageType;
+- (BOOL)isRequiredSoilType:(TileType)type;
+- (PlantType)plantType;
+- (BOOL)canDieSeason:(int)season;
+- (BOOL)floweringSeason:(int)season;
 - (int)minAllowedTemperature;
-- (int)seedItemType;
+- (ItemType)seedItemType;
 - (float)maxAgeBase;
 - (Vector)lightColor;
 - (float)lightFactor;
 - (BOOL)emitsLight;
-- (int)objectType;
+- (DynamicObjectType)objectType;
 
 @end
