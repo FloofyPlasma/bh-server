0180e4ebc9c9dff860ad7b7978eec94a8deddd21
FloofyPlasma <github@floofyplasma.com>
2025-05-26 16:48:02 -0400

    fix: mj multi sound object types
    #5

--- src/SharedSource/MJMultiSound.h
+++ src/SharedSource/MJMultiSound.h
@@ -27,28 +27,28 @@
 @property BOOL incrementalPitchOffset; // @synthesize incrementalPitchOffset;
 @property BOOL randomPitchOffset; // @synthesize randomPitchOffset;
 @property double lastPlayTime; // @synthesize lastPlayTime;
-- (void)setLoopLength:(double)arg1;
-- (void)preLoad:(unsigned int)arg1;
-- (void)setPlaybackFrequencyOfSoundAtIndex:(unsigned int)arg1
-                               toFrequency:(float)arg2;
-- (id)sound;
-- (void)setMaxPlaybacks:(int)arg1;
-- (void)setPitch:(float)arg1;
-- (void)setMasterVolumeMultiplier:(float)arg1;
+- (void)setLoopLength:(NSTimeInterval)loopLength_;
+- (void)preLoad:(unsigned int)count;
+- (void)setPlaybackFrequencyOfSoundAtIndex:(unsigned int)index
+                               toFrequency:(float)playbackFrequency;
+- (MJSound*)sound;
+- (void)setMaxPlaybacks:(int)maxPlaybacks;
+- (void)setPitch:(float)pitch;
+- (void)setMasterVolumeMultiplier:(float)multiplier;
 - (float)getLegacyVolume;
-- (void)setVolume:(float)arg1;
-- (void)setLegacyVolume:(float)arg1;
-- (void)setLooping:(BOOL)arg1;
+- (void)setVolume:(float)volume;
+- (void)setLegacyVolume:(float)volume;
+- (void)setLooping:(BOOL)looping;
 - (void)stopAll;
-- (void)playAtPosition:(Vector2)arg1 afterDelay:(double)arg2;
-- (void)playDelayedSoundWithDict:(id)arg1;
-- (void)playAtPosition:(Vector2)arg1;
+- (void)playAtPosition:(Vector2)pos afterDelay:(NSTimeInterval)delay;
+- (void)playDelayedSoundWithDict:(NSDictionary*)delayDict;
+- (void)playAtPosition:(Vector2)pos;
 - (void)stopLoopingSound;
-- (void)playAfterDelay:(double)arg1;
-- (void)backgroundThreadPlayWithDelay:(id)arg1;
+- (void)playAfterDelay:(NSTimeInterval)delay;
+- (void)backgroundThreadPlayWithDelay:(NSDictionary*)delayDict;
 - (void)play;
 - (void)dealloc;
-- (id)initWithFileNames:(id)arg1;
-- (id)initWithFile:(id)arg1;
+- (MJMultiSound*)initWithFileNames:(NSArray*)fileNames;
+- (MJMultiSound*)initWithFile:(NSString*)file;
 
 @end
