c27df786d35b45cb54417e56e6820cf453c75c3a
FloofyPlasma <github@floofyplasma.com>
2025-05-26 16:23:34 -0400

    fix: ridable dynamic object protocl object types
    #5

--- src/SharedSource/RidableDynamicObject-Protocol.h
+++ src/SharedSource/RidableDynamicObject-Protocol.h
@@ -1,6 +1,7 @@
 #import <Foundation/Foundation.h>
 #import <GLKit/GLKMathTypes.h>
 
+#import "InventoryItem.h"
 #import "Vector.h"
 #import "Vector2.h"
 
@@ -9,25 +10,25 @@
 @protocol RidableDynamicObject <NSObject>
 - (BOOL)riderDPadShouldGiveDiscreteValues;
 - (BOOL)riderDPadShouldAllowUpDown;
-- (void)blockheadUnloaded:(Blockhead*)arg1;
+- (void)blockheadUnloaded:(Blockhead*)blockhead;
 - (InteractionObject*)actsAsInteractionObject;
 - (BOOL)requiresFuel;
 - (BOOL)jumpsOnSwipe;
-- (BOOL)blockheadCanRide:(Blockhead*)arg1 usingItem:(int)arg2;
+- (BOOL)blockheadCanRide:(Blockhead*)blockhead usingItem:(ItemType)itemType;
 - (void)swipeUpGesture;
 - (void)blockheadsLoaded;
-- (void)removeRider:(Blockhead*)arg1;
-- (void)addRider:(Blockhead*)arg1;
+- (void)removeRider:(Blockhead*)blockhead;
+- (void)addRider:(Blockhead*)blockhead;
 - (int)rideDirection;
-- (void)setTargetVelocity:(Vector2)arg1;
-- (float)riderBodyYRotationForBlockhead:(Blockhead*)arg1;
+- (void)setTargetVelocity:(Vector2)targetVelocity;
+- (float)riderBodyYRotationForBlockhead:(Blockhead*)blockhead;
 - (Vector2)renderPos;
-- (Vector2)cameraPosForBlockhead:(Blockhead*)arg1;
-- (Vector)riderPosForBlockhead:(Blockhead*)arg1;
+- (Vector2)cameraPosForBlockhead:(Blockhead*)blockhead;
+- (Vector)riderPosForBlockhead:(Blockhead*)blockhead;
 
 @optional
 - (BOOL)riderRidesWithArmsDown;
 - (float)riderAnimationTimer;
-- (GLKMatrix4)riderBodyMatrixForBlockhead:(Blockhead*)arg1
-                                  cameraX:(float)arg2;
+- (GLKMatrix4)riderBodyMatrixForBlockhead:(Blockhead*)blockhead
+                                  cameraX:(float)cameraX;
 @end
