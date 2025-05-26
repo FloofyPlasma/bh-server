a853b19575e2d47cbb54b9236913d23898dd8e32
FloofyPlasma <github@floofyplasma.com>
2025-05-26 13:06:03 -0400

    fix: database object types
    #5

--- src/SharedSource/Database.h
+++ src/SharedSource/Database.h
@@ -5,20 +5,20 @@
 @interface Database : NSObject {
   DatabaseEnvironment* databaseEnvironment;
   struct MDB_env* env;
-  unsigned int dbi;
+  uint32_t dbi;
   NSString* name;
 }
 
-@property (readonly) unsigned int dbi; // @synthesize dbi;
-- (BOOL)hasKey:(id)arg1;
-- (id)dataForKey:(id)arg1 mutableData:(BOOL)arg2;
-- (id)mutableDataForKey:(id)arg1;
-- (id)dataForKey:(id)arg1;
-- (BOOL)removeDataForKey:(id)arg1;
-- (BOOL)setData:(id)arg1 forKey:(id)arg2;
-- (BOOL)finishTransaction:(struct MDB_txn*)arg1;
+@property (readonly) uint32_t dbi; // @synthesize dbi;
+- (BOOL)hasKey:(NSString*)key;
+- (NSData*)dataForKey:(NSString*)key mutableData:(BOOL)mutableData;
+- (NSMutableData*)mutableDataForKey:(NSString*)key;
+- (NSData*)dataForKey:(NSString*)key;
+- (BOOL)removeDataForKey:(NSString*)key;
+- (BOOL)setData:(NSData*)data forKey:(NSString*)key;
+- (BOOL)finishTransaction:(struct MDB_txn*)transaction;
 - (struct MDB_txn*)getTransaction;
 - (void)dealloc;
-- (id)initWithEnvironment:(id)arg1 name:(id)arg2;
+- (Database*)initWithEnvironment:(DatabaseEnvironment*)databaseEnvironment_ name:(NSString*)databaseName;
 
 @end
