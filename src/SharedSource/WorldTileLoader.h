a986f39696b6ac18fb2392edd6cf95e1130ed757
FloofyPlasma <github@floofyplasma.com>
2025-05-28 00:48:25 -0400

    fix: world tile loader object types
    #5

--- src/SharedSource/WorldTileLoader.h
+++ src/SharedSource/WorldTileLoader.h
@@ -1,7 +1,29 @@
 #import <Foundation/Foundation.h>
 
 #import "MJMath.h"
+#import "NPC.h"
+#import "Plant.h"
+#import "Tree.h"
 #import "World.h"
+
+enum IslandType {
+  ISLAND_NONE = 0x0,
+  ISLAND_AMETHYST = 0x1,
+  ISLAND_SAPPHIRE = 0x2,
+  ISLAND_EMERALD = 0x3,
+  ISLAND_RUBY = 0x4,
+  ISLAND_DIAMOND = 0x5,
+  ISLAND_APPLE = 0x6,
+  ISLAND_COCONUT = 0x7,
+  ISLAND_ORANGE = 0x8,
+  ISLAND_PINE = 0x9,
+  ISLAND_LIME = 0xA,
+  ISLAND_CACTUS = 0xB,
+  ISLAND_CHERRY = 0xC,
+  ISLAND_COFFEE = 0xD,
+  ISLAND_MAPLE = 0xE,
+  ISLAND_MANGO = 0xF,
+};
 
 @class Database, DatabaseEnvironment, NoiseFunction, World;
 
@@ -21,9 +43,9 @@
   NoiseFunction* caveNoiseFunctionA;
   NoiseFunction* caveNoiseFunctionB;
   NoiseFunction* gemNoiseFunction;
-  int* treePositions;
-  int* npcPositions;
-  int* plantPositions;
+  TreeType* treePositions;
+  NPCType* npcPositions;
+  PlantType* plantPositions;
   intpair bestStartPosition;
   intpair highestPoint;
   int* dirtHeights;
@@ -47,88 +69,88 @@
     int xFrequencyMultiplier; // @synthesize xFrequencyMultiplier;
 @property BOOL needsToExit; // @synthesize needsToExit;
 @property (readonly) intpair highestPoint; // @synthesize highestPoint;
-@property (readonly) int* plantPositions; // @synthesize plantPositions;
-@property (readonly) int* npcPositions; // @synthesize npcPositions;
-@property (readonly) int* treePositions; // @synthesize treePositions;
+@property (readonly) PlantType* plantPositions; // @synthesize plantPositions;
+@property (readonly) NPCType* npcPositions; // @synthesize npcPositions;
+@property (readonly) TreeType* treePositions; // @synthesize treePositions;
 @property (readonly) NoiseFunction* seasonOffsetNoiseFunction; // @synthesize seasonOffsetNoiseFunction;
 @property (readonly) NoiseFunction* treeDensityNoiseFunction; // @synthesize treeDensityNoiseFunction;
 @property (readonly) intpair bestStartPosition; // @synthesize bestStartPosition;
 @property (readonly) unsigned int randomSeed; // @synthesize randomSeed;
 - (BOOL)finishBulkLightBlockTransaction;
 - (void)startBulkLightBlockTransaction;
-- (void)saveLightBlockForClientLightBlockIndex:(int)arg1
-                                      clientID:(id)arg2
-                                 physicalBlock:(PhysicalBlock*)arg3
-                                       sendNow:(BOOL)arg4;
-- (BOOL)sendLightBlockToClientWithoutSavingForBlock:(PhysicalBlock*)arg1
-                                                pos:(intpair)arg2
-                                       sendToClient:(id)arg3
-                                             server:(id)arg4;
-- (void)loadLightBlockForClientLightBlockIndex:(int)arg1
-                                      clientID:(id)arg2
-                             intoPhysicalBlock:(PhysicalBlock*)arg3;
-- (void)archiveLightBlocksForClient:(id)arg1;
-- (void)unarchiveLightBlocksForClient:(id)arg1;
-- (void)updatePhysicalBlockToLatestVersion:(PhysicalBlock*)arg1;
-- (int*)distanceOrderedFoodTypes;
+- (void)saveLightBlockForClientLightBlockIndex:(int)clientLightBlockIndex
+                                      clientID:(NSString*)clientID
+                                 physicalBlock:(PhysicalBlock*)physicalBlock
+                                       sendNow:(BOOL)sendNow;
+- (BOOL)sendLightBlockToClientWithoutSavingForBlock:(PhysicalBlock*)physicalBlock
+                                                pos:(intpair)pos
+                                       sendToClient:(NSString*)client
+                                             server:(BHServer*)server;
+- (void)loadLightBlockForClientLightBlockIndex:(int)clientLightBlockIndex
+                                      clientID:(NSString*)clientID
+                             intoPhysicalBlock:(PhysicalBlock*)physicalBlock;
+- (void)archiveLightBlocksForClient:(NSString*)clientID;
+- (void)unarchiveLightBlocksForClient:(NSString*)clientID;
+- (void)updatePhysicalBlockToLatestVersion:(PhysicalBlock*)physicalBlock;
+- (FoodType*)distanceOrderedFoodTypes;
 - (intpair)findBestStartPosition;
-- (void)loadPhysicalBlock:(PhysicalBlock*)arg1
-                   atXPos:(int)arg2
-                     yPos:(int)arg3
-       createIfNotCreated:(BOOL)arg4;
-- (void)placeGemsInCaveForPhysicalBlock:(PhysicalBlock*)arg1
-                              tileIndex:(int)arg2
-                                 worldX:(int)arg3
-                                 worldY:(int)arg4
-                     floatingIslandType:(int)arg5;
-- (void)recursivelyFlowOutDirtFromTile:(Tile*)arg1 atPos:(intpair)arg2;
-- (void)recursivelyFlowOutWaterFromTile:(Tile*)arg1 atPos:(intpair)arg2;
-- (void)fillDirtTile:(Tile*)arg1
-            worldPos:(intpair)arg2
-     worldDirtHeight:(int)arg3
-          parentType:(int)arg4;
-- (BOOL)isDesertOrBeachForPos:(intpair)arg1 height:(int)arg2;
-- (BOOL)isBeachForPos:(intpair)arg1 height:(int)arg2;
-- (BOOL)isDesertForPos:(intpair)arg1 height:(int)arg2;
-- (float)sandFractionForPos:(intpair)arg1 height:(int)arg2 highRes:(BOOL)arg3;
-- (float)sandFractionForPos:(intpair)arg1 highRes:(BOOL)arg2;
-- (void)savePhysicalBlock:(PhysicalBlock*)arg1
-                macroTile:(MacroTile*)arg2
-            sendToClients:(id)arg3
-                   server:(id)arg4
-             sendReliably:(BOOL)arg5;
-- (BOOL)sendBlockToClientWithoutSavingForBlock:(PhysicalBlock*)arg1
-                                           pos:(intpair)arg2
-                                  sendToClient:(id)arg3
-                                        server:(id)arg4
-                            sendDynamicObjects:(BOOL)arg5
-                                      reliable:(BOOL)arg6;
-- (BOOL)isFloatingIslandCaveForX:(int)arg1 y:(int)arg2;
-- (BOOL)isCaveForX:(int)arg1 y:(int)arg2 faultOffset:(int)arg3;
-- (int)lakeHeightForX:(int)arg1;
-- (int)maxOfRockAndDirtHeightForX:(int)arg1;
-- (int)unmodifiedGroundLevelForX:(int)arg1;
-- (float)sandstoneFractionForX:(int)arg1
-                             y:(int)arg2
-                   faultOffset:(int)arg3
-             limestoneFraction:(float)arg4;
-- (float)limestoneFractionForX:(int)arg1 y:(int)arg2 faultOffset:(int)arg3;
-- (int)getCloudHeightForX:(int)arg1;
-- (void)getRockAndDirtHeightforX:(int)arg1
-                      rockHeight:(int*)arg2
-                      dirtHeight:(int*)arg3;
-- (int)faultOffsetForX:(int)arg1 y:(int)arg2;
-- (void)getInitialRockAndDirtHeightforX:(int)arg1
-                             rockHeight:(float*)arg2
-                             dirtHeight:(float*)arg3;
+- (void)loadPhysicalBlock:(PhysicalBlock*)physicalBlock
+                   atXPos:(int)xPos
+                     yPos:(int)yPos
+       createIfNotCreated:(BOOL)createIfNotCreated;
+- (void)placeGemsInCaveForPhysicalBlock:(PhysicalBlock*)physicalBlock
+                              tileIndex:(int)tileIndex
+                                 worldX:(int)worldX
+                                 worldY:(int)worldY
+                     floatingIslandType:(IslandType)floatingIslandType;
+- (void)recursivelyFlowOutDirtFromTile:(Tile*)tile atPos:(intpair)worldPos;
+- (void)recursivelyFlowOutWaterFromTile:(Tile*)tile atPos:(intpair)worldPos;
+- (void)fillDirtTile:(Tile*)tile
+            worldPos:(intpair)worldPos
+     worldDirtHeight:(int)worldDirtHeight
+          parentType:(TileType)parentType;
+- (BOOL)isDesertOrBeachForPos:(intpair)worldPos height:(int)height;
+- (BOOL)isBeachForPos:(intpair)worldPos height:(int)height;
+- (BOOL)isDesertForPos:(intpair)worldPos height:(int)height;
+- (float)sandFractionForPos:(intpair)worldPos height:(int)height highRes:(BOOL)highRes;
+- (float)sandFractionForPos:(intpair)worldPos highRes:(BOOL)highRes;
+- (void)savePhysicalBlock:(PhysicalBlock*)physicalBlock
+                macroTile:(MacroTile*)macroTile
+            sendToClients:(NSArray*)clientsToSend
+                   server:(BHServer*)server
+             sendReliably:(BOOL)sendReliably;
+- (BOOL)sendBlockToClientWithoutSavingForBlock:(PhysicalBlock*)physicalBlock
+                                           pos:(intpair)pos
+                                  sendToClient:(NSString*)clientToSend
+                                        server:(BHServer*)server
+                            sendDynamicObjects:(BOOL)sendDynamicObjects
+                                      reliable:(BOOL)reliable;
+- (BOOL)isFloatingIslandCaveForX:(int)x y:(int)y;
+- (BOOL)isCaveForX:(int)x y:(int)y faultOffset:(int)faultOffset;
+- (int)lakeHeightForX:(int)x;
+- (int)maxOfRockAndDirtHeightForX:(int)x;
+- (int)unmodifiedGroundLevelForX:(int)x;
+- (float)sandstoneFractionForX:(int)worldX
+                             y:(int)worldY
+                   faultOffset:(int)faultOffset
+             limestoneFraction:(float)limestoneFraction;
+- (float)limestoneFractionForX:(int)worldX y:(int)worldY faultOffset:(int)faultOffset;
+- (int)getCloudHeightForX:(int)x;
+- (void)getRockAndDirtHeightforX:(int)x
+                      rockHeight:(int*)rockHeight
+                      dirtHeight:(int*)dirtHeight;
+- (int)faultOffsetForX:(int)x y:(int)y;
+- (void)getInitialRockAndDirtHeightforX:(int)x
+                             rockHeight:(float*)rockHeight
+                             dirtHeight:(float*)dirtHeight;
 - (void)refineTerrain;
 - (void)dealloc;
 - (void)compressBlocks;
-- (id)initWithWorld:(id)arg1
-         randomSeed:(unsigned int)arg2
-         isNewWorld:(BOOL)arg3
-             saveID:(id)arg4
-      loadedVersion:(int)arg5
-      blockDatabase:(id)arg6;
+- (WorldTileLoader*)initWithWorld:(World*)world_
+                       randomSeed:(unsigned int)randomSeed_
+                       isNewWorld:(BOOL)isNewWorld
+                           saveID:(NSString*)saveID
+                    loadedVersion:(int)loadedVersion
+                    blockDatabase:(Database*)blockDatabase_;
 
 @end
