281e184eff8783a1ac1987672b7cb5be5443618e
FloofyPlasma <github@floofyplasma.com>
2025-08-13 18:21:28 -0400

    feat: add dependency management for enet and lmdb in CMake configuration

---
+++ Dependencies.cmake
@@ -0,0 +1,28 @@
+include(cmake/CPM.cmake)
+CPMUsePackageLock(package-lock.cmake)
+
+function(bhServer_setup_dependencies)
+
+    if(NOT TARGET enet)
+        cpmaddpackage("gh:lsalzman/enet@1.3.18")
+    endif()
+
+    if(NOT TARGET lmdb)
+        cpmaddpackage(
+            NAME lmdb
+            GITHUB_REPOSITORY LMDB/lmdb
+            GIT_TAG "LMDB_0.9.31"
+            DOWNLOAD_ONLY YES
+        )
+
+        add_library(lmdb STATIC
+            ${lmdb_SOURCE_DIR}/libraries/liblmdb/mdb.c
+            ${lmdb_SOURCE_DIR}/libraries/liblmdb/midl.c
+        )
+
+        target_include_directories(lmdb PUBLIC
+            ${lmdb_SOURCE_DIR}/libraries/liblmdb
+        )
+    endif()
+
+endfunction()
---
+++ package-lock.cmake
@@ -0,0 +1,17 @@
+# CPM Package Lock
+# This file should be committed to version control
+
+# enet
+CPMDeclarePackage(enet
+  VERSION 1.3.18
+  GITHUB_REPOSITORY lsalzman/enet
+  SYSTEM YES
+  EXCLUDE_FROM_ALL YES
+)
+# lmdb (unversioned)
+# CPMDeclarePackage(lmdb
+#  NAME lmdb
+#  GIT_TAG LMDB_0.9.31
+#  DOWNLOAD_ONLY YES
+#  GITHUB_REPOSITORY LMDB/lmdb
+#)
