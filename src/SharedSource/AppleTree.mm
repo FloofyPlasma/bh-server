b044c69805e0b65a4f1a5d1f3c1e4a2ee91992ec
FloofyPlasma <github@floofyplasma.com>
2025-09-04 18:44:53 -0400

    refactor: improve documentation in AppleTree

--- src/SharedSource/AppleTree.mm
+++ src/SharedSource/AppleTree.mm
@@ -4,90 +4,77 @@
 
 @implementation AppleTree
 
-- (AppleTree*)initWithWorld:(World*)world_ dynamicWorld:(DynamicWorld*)dynamicWorld_ atPosition:(intpair)pos_ cache:(CPCache*)cache_ maxHeight:(int16_t)maxHeight_ growthRate:(int16_t)growthRate_ treeDensityNoiseFunction:(NoiseFunction*)treeDensityNoiseFunction_ seasonOffsetNoiseFunction:(NoiseFunction*)seasonOffsetNoiseFunction_ adultTree:(BOOL)adultTree adultMaxAge:(float)adultMaxAge
-{
-  return nil;
+- (AppleTree *)initWithWorld:(World *)world_ dynamicWorld:(DynamicWorld *)dynamicWorld_ atPosition:(intpair)pos_ cache:(CPCache *)cache_ maxHeight:(int16_t)maxHeight_ growthRate:(int16_t)growthRate_ treeDensityNoiseFunction:(NoiseFunction *)treeDensityNoiseFunction_ seasonOffsetNoiseFunction:(NoiseFunction *)seasonOffsetNoiseFunction_ adultTree:(BOOL)adultTree adultMaxAge:(float)adultMaxAge {
+    return nil;
 }
 
-- (AppleTree*)initWithWorld:(World*)world_ dynamicWorld:(DynamicWorld*)dynamicWorld_ atPosition:(intpair)pos_ cache:(CPCache*)cache_ treeDensityNoiseFunction:(NoiseFunction*)treeDensityNoiseFunction_ seasonOffsetNoiseFunction:(NoiseFunction*)seasonOffsetNoiseFunction_
-{
-  return nil;
+- (AppleTree *)initWithWorld:(World *)world_ dynamicWorld:(DynamicWorld *)dynamicWorld_ atPosition:(intpair)pos_ cache:(CPCache *)cache_ treeDensityNoiseFunction:(NoiseFunction *)treeDensityNoiseFunction_ seasonOffsetNoiseFunction:(NoiseFunction *)seasonOffsetNoiseFunction_ {
+    return nil;
 }
 
-- (BOOL)fruitShouldFallInSeason:(int)season
-{
-  return season == 3;
+- (BOOL)fruitShouldFallInSeason:(int)season {
+    return season == 3;
 }
 
-- (BOOL)tileIsKindOfSelf:(Tile*)tile
-{
-  switch (tile->contents) {
-  case CONTENTS_APPLE_TREE_BUSH_DEAD:
-  case CONTENTS_APPLE_TREE_BUSH:
-  case CONTENTS_APPLE_TREE_TRUNK:
-  case CONTENTS_APPLE_TREE_TRUNK_BUSH:
-  case CONTENTS_APPLE_TREE_TRUNK_DEAD:
-    return YES;
-  default:
-    return NO;
-  }
+- (BOOL)tileIsKindOfSelf:(Tile *)tile {
+    switch (tile->contents) {
+        case CONTENTS_APPLE_TREE_BUSH_DEAD:
+        case CONTENTS_APPLE_TREE_BUSH:
+        case CONTENTS_APPLE_TREE_TRUNK:
+        case CONTENTS_APPLE_TREE_TRUNK_BUSH:
+        case CONTENTS_APPLE_TREE_TRUNK_DEAD:
+            return YES;
+        default:
+            return NO;
+    }
 }
 
-- (DynamicObjectType)objectType
-{
-  return DYNAMIC_OBJECT_TYPE_APPLE_TREE;
+- (DynamicObjectType)objectType {
+    return DYNAMIC_OBJECT_TYPE_APPLE_TREE;
 }
 
-- (ItemType)fruitItemType
-{
-  return ITEM_APPLE;
+- (ItemType)fruitItemType {
+    return ITEM_APPLE;
 }
 
-- (NSMutableDictionary*)getSaveDict
-{
-  NSMutableDictionary* dict = [super getSaveDict];
+- (NSMutableDictionary *)getSaveDict {
+    NSMutableDictionary *dict = [super getSaveDict];
 
-  dict[@"availableFood"] = @(self->availableFood);
+    dict[@"availableFood"] = @(self->availableFood);
 
-  return dict;
+    return dict;
 }
 
-- (TreeType)treeType
-{
-  return TREE_APPLE;
+- (TreeType)treeType {
+    return TREE_APPLE;
 }
 
-- (float)availableFood
-{
-  return self->availableFood;
+- (float)availableFood {
+    return self->availableFood;
 }
 
-- (void)makeTileDead:(Tile*)tile
-{
-  if (tile->contents == CONTENTS_APPLE_TREE_BUSH) {
-    tile->contents = CONTENTS_APPLE_TREE_BUSH_DEAD;
-  } else if (tile->contents == CONTENTS_APPLE_TREE_TRUNK || tile->contents == CONTENTS_APPLE_TREE_TRUNK_BUSH) {
-    tile->contents = CONTENTS_APPLE_TREE_TRUNK_DEAD;
-  }
+- (void)makeTileDead:(Tile *)tile {
+    if (tile->contents == CONTENTS_APPLE_TREE_BUSH) {
+        tile->contents = CONTENTS_APPLE_TREE_BUSH_DEAD;
+    } else if (tile->contents == CONTENTS_APPLE_TREE_TRUNK || tile->contents == CONTENTS_APPLE_TREE_TRUNK_BUSH) {
+        tile->contents = CONTENTS_APPLE_TREE_TRUNK_DEAD;
+    }
 }
 
-- (void)setAvailableFood:(float)food
-{
-  self->availableFood = food;
-  [self->dynamicWorld dynamicWorldChangedAtPos:[self pos] objectType:[self objectType]];
+- (void)setAvailableFood:(float)food {
+    self->availableFood = food;
+    [self->dynamicWorld dynamicWorldChangedAtPos:[self pos] objectType:[self objectType]];
 }
 
-- (void)update:(float)dt accurateDT:(float)accurateDT isSimulation:(BOOL)isSimulation
-{
-  [super update:dt accurateDT:accurateDT isSimulation:isSimulation];
+- (void)update:(float)dt accurateDT:(float)accurateDT isSimulation:(BOOL)isSimulation {
+    [super update:dt accurateDT:accurateDT isSimulation:isSimulation];
 
-  if (!self->dead) {
-    // TODO:
-  }
+    if (!self->dead) {
+        // TODO:
+    }
 }
 
-- (void)updateGrowth:(BOOL)addNewBranchBlocks
-{
+- (void)updateGrowth:(BOOL)addNewBranchBlocks {
 }
 
 @end
